c4d521ec8612d9fd576dd91c2b71a886bf0c53f1
drolsonmi <dr.olsonmi@gmail.com>
2019-03-06 18:34:29 -0500

    Updating IP.sh

--- RaspberryPiTutorials/IP.sh
+++ RaspberryPiTutorials/IP.sh
@@ -1,5 +1,14 @@
 #!/bash/bin
 
+# To automate this, run the following commands
+# on the Raspberry Pi:
+#
+# git config credential.helper store
+# git push origin master
+#
+# Enter credentials, then you won't need to for
+# the next login
+
 git pull origin master
 ip a > ip_address.txt
 git add ip_address.txt
